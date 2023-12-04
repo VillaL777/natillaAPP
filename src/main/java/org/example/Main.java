819c0761105938d853dc87f4268cf836d8798df7
juanbu17 <140201939+juanbu17@users.noreply.github.com>
2023-12-04 15:34:47 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreUsuario = "Luis Miguel Villa Perez";
+        short edadUsuario = 19;
+        String telefonoUsuario = "3113368435";
+        String documentoIdentidad = "1045418105";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario = "Av 47 #60-35";
+        double costoInscripcion = 150000.0;
+        float tasaInteresNatillera = 0.1F;
+        double saldoFinalUsuario;
+
+
+        //PROCESO
+
+        // 1. Asignar un valor especifico a una variable
+        // desde el TECLADO (recibir datos)
+
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+
+
+    }
+    }
+
+
+
